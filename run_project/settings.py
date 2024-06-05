6f684a05ea358b99a079de148148c7bf78b24431
Danny Julca Carhuaz <djulcac@uni.pe>
2024-06-05 13:21:34 -0500

    feat: move-package

--- run_project/settings.py
+++ run_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_accounts.accounts_base',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+## CUSTOM USERS
+AUTH_USER_MODEL = "accounts_base.User"
