ec2aa910cf8bea2cf6fced431cfa03e5a854986d
Danny Julca Carhuaz <djulcac@uni.pe>
2024-06-17 13:35:39 -0500

    feat: config run_project and style password

--- django_accounts/accounts_sun/api/serializers.py
+++ django_accounts/accounts_sun/api/serializers.py
@@ -3,4 +3,6 @@ from rest_framework import serializers
 
 class LoginSerializer(serializers.Serializer):
     identifier = serializers.CharField()
-    password = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_type': 'password', 'placeholder': 'Password'}
+    )
